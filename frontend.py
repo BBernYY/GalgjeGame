cb58a7d8e277073a67cad0b9f72abb0b161ae19a
BBernYY <66414852+BBernYY@users.noreply.github.com>
2021-07-20 15:11:35 +0200

    Debugged errors, made full english

--- frontend.py
+++ frontend.py
@@ -1,10 +1,14 @@
 # lives is een int met amount levens, letters_wrong is een list met alle verkeerde letters, en word is iets zoals a__le
 # probeer een in een prompt zo goed mogelijk deze variabelen te zien
-def display_round(lives, words_wrong, word):
+def func(lives, letters_wrong, word):
     # code
-    print("Je hebt nog ", lives, "levens")
-    print("Deze letters zitten er niet in: ", letters_wrong)
-    return input("ask what letter the player wants to guess" + "\n")
+    string = ""
+    for i in list(word):
+        string += i+" "
+    print(string)
+    print("You've got", lives, "lives")
+    print("These letters aren't in there:", letters_wrong)
+    return input("Please guess a letter." + "\n")
      
     
     
--- main.py
+++ main.py
@@ -2,34 +2,35 @@ import random
 import frontend as f
 import english_words as ew
 def round_check(lives, no_letters, word, wordP):
-    running = True
-    if lives == 0:
-        event("die")
+    if lives == 1:
+        event("die", word)
     elif not "_" in wordP:
-        event("win")
+        event("win", word)
     else:
         return getword(no_letters, word, wordP, f.func(lives, no_letters, wordP), lives)
 def getword(no_letters, word, wordP, letter, lives):
     if letter in word:
-        wordP[word.index(letter)] = letter
+        s = list(wordP)
+        s[word.index(letter)] = letter
+        wordP = "".join(s)
     else:
         no_letters.append(letter)
         lives -= 1
     return {"no_letters": no_letters, "word": word, "wordP": wordP, "lives": lives}
-def event(e_type):
+def event(e_type, word):
     global running
     if e_type == "die":
         print("You died... Sadly...")
+        print("The word was "+word)
     else:
         print("GG! well played!")
     print("Starting next round\n\n")
     running = False
 while True:
     d = {}
-    d["no_letters"], d["word"], d["wordP"], d["lives"] = 11, [], random.choice(list(ew.english_words_lower_alpha_set)), ""
-    for i in len(d["word"]):
+    d["no_letters"], d["word"], d["wordP"], d["lives"] = [], random.choice(list(ew.english_words_lower_alpha_set)), "", 11
+    for i in range(len(d["word"])):
         d["wordP"] += "_"
+    running = True
     while running:
         d = round_check(d["lives"], d["no_letters"], d["word"], d["wordP"])
-        
-        
